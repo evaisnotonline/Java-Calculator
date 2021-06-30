de19db57a01e6d29c2067cbd17c021f6af2d93f7
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-06-30 11:24:41 +0100

    adding calculator

---
+++ Operators.java
@@ -0,0 +1,24 @@
+package com.qa.helloworld;
+
+public class Operators {
+
+	public static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+
+	public static int times(int a, int b) {
+		int times = a * b;
+		return times;
+	}
+
+	public static int subtract(int a, int b) {
+		int subtract = a - b;
+		return subtract;
+	}
+
+	public static double divide(double a, double b) {
+		double divide = a / b;
+		return divide;
+	}
+}
